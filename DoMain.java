473ff14f963d334bc91b71a51cf66911d5d99f17
couhehehe <abcd724152040@qq.com>
2017-04-08 14:24:24 +0800

    Add files via upload

---
+++ DoMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cou.testcase.main;
+
+/**
+ * @author cou
+ *
+ */
+public class DoMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("fsdaf");
+	}
+
+}
